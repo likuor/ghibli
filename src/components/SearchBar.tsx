e9774d40bbd02767501ce19625e498dcd0704995
Koki Sakai <likuor0528@gmail.com>
2022-08-02 17:05:30 -0700

    Fix bugs for netlify

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,12 +1,12 @@
 import { Input, Space } from 'antd';
-import { useSelector, useDispatch } from 'react-redux';
+// import { useSelector, useDispatch } from 'react-redux';
 
 const { Search } = Input;
 const onSearch = (value: string) => console.log(value);
 
 const SearchBar: React.FC = () => {
-  const redux = useSelector((state: any) => state);
-  const dispatch = useDispatch();
+  // const redux = useSelector((state: any) => state);
+  // const dispatch = useDispatch();
   // console.log(redux);
 
   return (
